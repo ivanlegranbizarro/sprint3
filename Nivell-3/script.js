74f03168514b241649b03cd43a0ebbcf41da80c0
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-11-08 10:02:12 +0100

    últims retocs Nivell3

--- Nivell-3/script.js
+++ Nivell-3/script.js
@@ -1,39 +1,75 @@
 /* Escriu una aplicació que creï diferents objectes Usuari/ària. L'aplicació podrà crear diferents Temes i subscriure els usuaris/es a ells. Quan un Usuari/ària afegeixi un missatge a un Tema s'enviarà una alerta per la consola des del Tema. També ho mostraran per consola cadascun dels Usuaris/es que estiguin subscrits al Tema (rebran el missatge). Crea un Tema amb un Usuari/ària i un altre amb dos i mostra la recepció dels missatges pels usuaris/es. Utilitza el mòdul events. */
 
-class Publicador {
-  constructor() {
-    this.subscriptors = []
+class Tema {
+  constructor ( titol ) {
+    this.titol = titol;
+    this.usuaris = [];
+    this.missatges = [];
   }
 
-
-  subscribe(subscriptor) {
-    this.subscriptors.push(subscriptor);
+  afegirUsuari ( usuari ) {
+    this.usuaris.push( usuari );
   }
 
-
-  unsubscribe(subscriptor) {
-    this.subscriptors = this.subscriptors.filter(
-      (item) => item !== subscriptor
-    );
+  afegirMissatge ( missatge ) {
+    this.missatges.push( missatge );
+    this.usuaris.forEach( ( usuari ) => {
+      usuari.rebreMissatge( missatge );
+    } );
   }
 
-
-  notify(event) {
-    this.subscriptors.forEach((item) => {
-      item.buzon.call(item, event);
-    });
+  usuarisSubscrits () {
+    console.log( `Usuaris subscrits al tema ${ this.titol }:` );
+    this.usuaris.forEach( ( usuari ) => {
+      console.log( usuari.nom );
+    } );
   }
+
 }
 
+class Usuari {
+  constructor ( nom ) {
+    this.nom = nom;
+  }
 
-class Subscriptor {
-  constructor(id) {
-    this.id = id;
-    console.log("Se ha creado el subscriptor #: " + id);
+  rebreMissatge ( missatge ) {
+    console.log( `${ this.nom } ha rebut el missatge: ${ missatge }` );
   }
-  buzon(edicion) {
-    console.log(
-      "Subscriptor # " + this.id + " recibió una nueva edición: " + edicion
-    );
+
+  subscriureTema ( tema ) {
+    tema.afegirUsuari( this );
   }
+
+  afegirMissatge ( tema, missatge ) {
+    tema.afegirMissatge( missatge );
+  }
+
 }
+
+const videojocs = new Tema( 'Videojocs' );
+const cuina = new Tema( 'Cuina' );
+
+
+const joan = new Usuari( 'Joan' );
+const maria = new Usuari( 'Maria' );
+const carles = new Usuari( 'Carles' );
+
+joan.subscriureTema( videojocs );
+
+maria.subscriureTema( videojocs );
+
+carles.subscriureTema( videojocs );
+
+carles.subscriureTema( cuina );
+
+joan.afegirMissatge( videojocs, 'Hola, aquest és el primer missatge' );
+
+maria.afegirMissatge( videojocs, 'Hola, aquest és el segon missatge' );
+
+carles.afegirMissatge( videojocs, 'Hola, aquest és el tercer missatge' );
+
+carles.afegirMissatge( cuina, 'Hola, aquest és el primer missatge de cuina' );
+
+videojocs.usuarisSubscrits();
+
+cuina.usuarisSubscrits();
