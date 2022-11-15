b80c7362efe1101848c6fd59f0f49f2c622c70b3
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-11-15 09:13:45 +0100

    setup nivel 3

---
+++ Entrega-2/Nivell-3/app.js
@@ -0,0 +1,1 @@
+/* Utilitzant RabbitMQ com a element imprescindible crea una queue on una classe Publisher publiqui missatges que siguin llegits per una classe Subscriber. Mostra l'emissió i recepció de cada missatge en consoles diferents. */
